01334e319ba3225608b0eb76eda56e45b3e56906
tnk2tjs <tnk2tjs.gmail.com>
2017-03-12 00:38:47 +0900

    ちんこ

---
+++ script.js
@@ -0,0 +1,20 @@
+$(function(){
+  $(".unko").on("click",function(){
+    $("li.selected").removeClass("selected");
+    $("#tabmenu_1").addClass("selected");
+
+    $("#content > div").hide();
+    $("#tabcontent_1").fadeIn();
+  })
+  $("ul").on("click","li",function(){
+    //selectedのつけたり外したり
+    $("li.selected").removeClass("selected");
+    $(this).addClass("selected");
+
+    //tabのIDを取得
+    tabID=this.id.split("_")[1];
+    $("#content > div").hide();
+    $("#tabcontent_" + tabID).fadeIn();
+  })
+  $("#tabmenu_1").trigger("click");
+});
